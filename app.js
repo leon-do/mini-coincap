420a01b68d7851ec85e341e8ba4bd629847e8f9b
Leon <leondo123@gmail.com>
2017-06-12 11:40:23 -0400

    readme

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var socket = io.connect('https://socket.coincap.io');
+var socket = io.connect('http://socket.coincap.io/');
 
 socket.on('trades', function (tradeMsg) {
 	const myCoinArray = ['ETH', 'DASH', 'ETC', 'REP', 'LTC', 'ICN', 'GNT', 'GNO', 'DGD', 'BCAP']
 
+	console.log(tradeMsg)
     let coin = tradeMsg.message.coin
     let price = tradeMsg.message.msg.price
     let cap24hrChange = tradeMsg.message.msg.cap24hrChange
