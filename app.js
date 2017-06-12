79f21264ec41368b0243d045518e20d8b9937494
Leon <leondo123@gmail.com>
2017-06-12 11:24:33 -0400

    initial

---
+++ app.js
@@ -0,0 +1,17 @@
+var socket = io.connect('http://socket.coincap.io');
+
+socket.on('trades', function (tradeMsg) {
+	const myCoinArray = ['ETH', 'DASH', 'ETC', 'REP', 'LTC', 'ICN', 'GNT', 'GNO', 'DGD', 'BCAP']
+
+    let coin = tradeMsg.message.coin
+    let price = tradeMsg.message.msg.price
+    let cap24hrChange = tradeMsg.message.msg.cap24hrChange
+
+    if (myCoinArray.indexOf(coin) >= 0){
+    	$(`#${coin}_price`).text(price)
+    	$(`#${coin}_24h`).text(cap24hrChange)    	
+    }
+
+})
+
+
